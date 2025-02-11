3d4bf2f4f2b83f3baa323b80ecfd0c866c36f815
Aubrey Leary <laxleary18@gmail.com>
2025-02-11 10:50:48 -0500

    category analysis

--- outputs/networkx_graph.ml
+++ outputs/networkx_graph.ml
@@ -1,276 +1,279 @@
 <?xml version='1.0' encoding='utf-8'?>
-<graphml xmlns="http://graphml.graphdrawing.org/xmlns" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd"><key id="d0" for="node" attr.name="label" attr.type="string"/>
-<graph edgedefault="undirected"><node id="1">
-  <data key="d0">Acid Chlorides</data>
-</node>
-<node id="3">
-  <data key="d0">Acrylates/Methacrylates (Acute toxicity)</data>
-</node>
-<node id="4">
-  <data key="d0">Aldehydes (Acute toxicity)</data>
-</node>
-<node id="5">
-  <data key="d0">Aliphatic Amines</data>
-</node>
-<node id="6">
-  <data key="d0">Aluminum Compounds</data>
-</node>
-<node id="7">
-  <data key="d0">Anilines (Acute toxicity)</data>
-</node>
-<node id="8">
-  <data key="d0">Azides (Acute toxicity)</data>
-</node>
-<node id="9">
-  <data key="d0">Benzotriazoles (Acute toxicity)</data>
-</node>
-<node id="10">
-  <data key="d0">Benzotriazole-hindered phenols</data>
-</node>
-<node id="11">
-  <data key="d0">Boron Compounds</data>
-</node>
-<node id="15">
-  <data key="d0">Epoxides</data>
-</node>
-<node id="16">
-  <data key="d0">Esters (Acute toxicity)</data>
-</node>
-<node id="17">
-  <data key="d0">Hydrazines and Related Compounds</data>
-</node>
-<node id="18">
-  <data key="d0">Hindered Amines</data>
-</node>
-<node id="19">
-  <data key="d0">Imides (Acute toxicity)</data>
-</node>
-<node id="21">
-  <data key="d0">Neutral Organics</data>
-</node>
-<node id="23">
-  <data key="d0">Nonionic Surfactants</data>
-</node>
-<node id="25">
-  <data key="d0">Peroxides</data>
-</node>
-<node id="26">
-  <data key="d0">Phenolphthaleins</data>
-</node>
-<node id="27">
-  <data key="d0">Phenols (Acute toxicity)</data>
-</node>
-<node id="29">
-  <data key="d0">Phosphinate Esters (Acute toxicity)</data>
-</node>
-<node id="31">
-  <data key="d0">Thiols (Acute toxicity)</data>
-</node>
-<node id="32">
-  <data key="d0">Substituted Triazines (Acute toxicity)</data>
-</node>
-<node id="33">
-  <data key="d0">Triarylmethane Pigments/Dyes with Non-solubilizing Groups</data>
-</node>
-<node id="34">
-  <data key="d0">Vinyl Esters (Acute toxicity)</data>
-</node>
-<node id="35">
-  <data key="d0">Vinyl Sulfones</data>
-</node>
-<node id="37">
-  <data key="d0">Anhydrides, Carboxylic acid</data>
-</node>
-<node id="39">
-  <data key="d0">Diisocyanates</data>
-</node>
-<node id="40">
-  <data key="d0">Alkoxysilanes</data>
-</node>
-<node id="41">
-  <data key="d0">Dianilines</data>
-</node>
-<node id="42">
-  <data key="d0">Anionic Surfactants</data>
-</node>
-<node id="43">
-  <data key="d0">Dithiocarbamates (Acute toxicity)</data>
-</node>
-<node id="45">
-  <data key="d0">Polynitroaromatics (Acute toxicity)</data>
-</node>
-<node id="46">
-  <data key="d0">Rosin</data>
-</node>
-<node id="48">
-  <data key="d0">Aminobenzothiazole Azo Dyes</data>
-</node>
-<node id="49">
-  <data key="d0">Acrylates/Methacrylates (Chronic toxicity)</data>
-</node>
-<node id="50">
-  <data key="d0">Aldehydes (Chronic toxicity)</data>
-</node>
-<node id="51">
-  <data key="d0">Anilines (Chronic toxicity)</data>
-</node>
-<node id="53">
-  <data key="d0">Benzotriazoles (Chronic toxicity)</data>
-</node>
-<node id="55">
-  <data key="d0">Esters (Chronic toxicity)</data>
-</node>
-<node id="56">
-  <data key="d0">Imides (Chronic toxicity)</data>
-</node>
-<node id="58">
-  <data key="d0">Phenols (Chronic toxicity)</data>
-</node>
-<node id="61">
-  <data key="d0">Substituted Triazines (Chronic toxicity)</data>
-</node>
-<node id="62">
-  <data key="d0">Thiols (Chronic toxicity)</data>
-</node>
-<node id="63">
-  <data key="d0">Vinyl Esters (Chronic toxicity)</data>
-</node>
-<node id="65">
-  <data key="d0">Ethylene Glycol Ethers</data>
-</node>
-<edge source="1" target="7"/>
-<edge source="1" target="27"/>
-<edge source="1" target="45"/>
-<edge source="1" target="16"/>
-<edge source="1" target="21"/>
-<edge source="1" target="37"/>
-<edge source="3" target="9"/>
-<edge source="3" target="10"/>
-<edge source="3" target="16"/>
-<edge source="3" target="27"/>
-<edge source="3" target="21"/>
-<edge source="3" target="42"/>
-<edge source="3" target="15"/>
-<edge source="3" target="37"/>
-<edge source="3" target="34"/>
-<edge source="3" target="32"/>
-<edge source="3" target="40"/>
-<edge source="3" target="18"/>
-<edge source="4" target="21"/>
-<edge source="4" target="27"/>
-<edge source="4" target="16"/>
-<edge source="4" target="7"/>
-<edge source="4" target="11"/>
-<edge source="4" target="42"/>
-<edge source="4" target="23"/>
-<edge source="5" target="16"/>
-<edge source="5" target="21"/>
-<edge source="6" target="16"/>
-<edge source="7" target="27"/>
-<edge source="7" target="16"/>
-<edge source="7" target="32"/>
-<edge source="7" target="35"/>
-<edge source="7" target="41"/>
-<edge source="7" target="31"/>
-<edge source="7" target="45"/>
-<edge source="7" target="55"/>
-<edge source="7" target="17"/>
-<edge source="7" target="26"/>
-<edge source="7" target="37"/>
-<edge source="7" target="19"/>
-<edge source="7" target="40"/>
-<edge source="8" target="40"/>
-<edge source="9" target="10"/>
-<edge source="9" target="27"/>
-<edge source="9" target="16"/>
-<edge source="9" target="45"/>
-<edge source="10" target="27"/>
-<edge source="10" target="53"/>
-<edge source="10" target="55"/>
-<edge source="10" target="16"/>
-<edge source="10" target="58"/>
-<edge source="11" target="16"/>
-<edge source="15" target="19"/>
-<edge source="15" target="16"/>
-<edge source="15" target="21"/>
-<edge source="15" target="55"/>
-<edge source="15" target="18"/>
-<edge source="15" target="40"/>
-<edge source="15" target="32"/>
-<edge source="16" target="27"/>
-<edge source="16" target="45"/>
-<edge source="16" target="21"/>
-<edge source="16" target="37"/>
-<edge source="16" target="48"/>
-<edge source="16" target="32"/>
-<edge source="16" target="26"/>
-<edge source="16" target="40"/>
-<edge source="16" target="31"/>
-<edge source="16" target="42"/>
-<edge source="16" target="34"/>
-<edge source="16" target="29"/>
-<edge source="16" target="18"/>
-<edge source="16" target="25"/>
-<edge source="17" target="43"/>
-<edge source="17" target="45"/>
-<edge source="17" target="27"/>
-<edge source="17" target="18"/>
-<edge source="17" target="21"/>
-<edge source="17" target="19"/>
-<edge source="18" target="56"/>
-<edge source="18" target="58"/>
-<edge source="18" target="55"/>
-<edge source="18" target="32"/>
-<edge source="18" target="21"/>
-<edge source="19" target="39"/>
-<edge source="19" target="37"/>
-<edge source="19" target="40"/>
-<edge source="21" target="55"/>
-<edge source="21" target="27"/>
-<edge source="21" target="37"/>
-<edge source="21" target="58"/>
-<edge source="21" target="49"/>
-<edge source="21" target="26"/>
-<edge source="21" target="42"/>
-<edge source="21" target="34"/>
-<edge source="21" target="63"/>
-<edge source="21" target="50"/>
-<edge source="21" target="25"/>
-<edge source="21" target="23"/>
-<edge source="21" target="65"/>
-<edge source="21" target="46"/>
-<edge source="23" target="65"/>
-<edge source="25" target="42"/>
-<edge source="26" target="27"/>
-<edge source="27" target="31"/>
-<edge source="27" target="53"/>
-<edge source="27" target="55"/>
-<edge source="27" target="45"/>
-<edge source="27" target="49"/>
-<edge source="27" target="61"/>
-<edge source="27" target="33"/>
-<edge source="27" target="50"/>
-<edge source="27" target="32"/>
-<edge source="27" target="40"/>
-<edge source="31" target="42"/>
-<edge source="31" target="32"/>
-<edge source="31" target="55"/>
-<edge source="31" target="40"/>
-<edge source="32" target="35"/>
-<edge source="32" target="40"/>
-<edge source="32" target="39"/>
-<edge source="37" target="40"/>
-<edge source="39" target="56"/>
-<edge source="39" target="40"/>
-<edge source="42" target="55"/>
-<edge source="46" target="55"/>
-<edge source="49" target="55"/>
-<edge source="51" target="55"/>
-<edge source="53" target="55"/>
-<edge source="55" target="56"/>
-<edge source="55" target="58"/>
-<edge source="55" target="61"/>
-<edge source="55" target="63"/>
-<edge source="55" target="62"/>
-<edge source="58" target="61"/>
-</graph></graphml>
+<graphml xmlns="http://graphml.graphdrawing.org/xmlns" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
+  <key id="d0" for="node" attr.name="label" attr.type="string" />
+  <graph edgedefault="undirected">
+    <node id="1">
+      <data key="d0">Acid Chlorides</data>
+    </node>
+    <node id="3">
+      <data key="d0">Acrylates/Methacrylates (Acute toxicity)</data>
+    </node>
+    <node id="4">
+      <data key="d0">Aldehydes (Acute toxicity)</data>
+    </node>
+    <node id="5">
+      <data key="d0">Aliphatic Amines</data>
+    </node>
+    <node id="6">
+      <data key="d0">Aluminum Compounds</data>
+    </node>
+    <node id="7">
+      <data key="d0">Anilines (Acute toxicity)</data>
+    </node>
+    <node id="8">
+      <data key="d0">Azides (Acute toxicity)</data>
+    </node>
+    <node id="9">
+      <data key="d0">Benzotriazoles (Acute toxicity)</data>
+    </node>
+    <node id="10">
+      <data key="d0">Benzotriazole-hindered phenols</data>
+    </node>
+    <node id="11">
+      <data key="d0">Boron Compounds</data>
+    </node>
+    <node id="15">
+      <data key="d0">Epoxides</data>
+    </node>
+    <node id="16">
+      <data key="d0">Esters (Acute toxicity)</data>
+    </node>
+    <node id="17">
+      <data key="d0">Hydrazines and Related Compounds</data>
+    </node>
+    <node id="18">
+      <data key="d0">Hindered Amines</data>
+    </node>
+    <node id="19">
+      <data key="d0">Imides (Acute toxicity)</data>
+    </node>
+    <node id="21">
+      <data key="d0">Neutral Organics</data>
+    </node>
+    <node id="23">
+      <data key="d0">Nonionic Surfactants</data>
+    </node>
+    <node id="25">
+      <data key="d0">Peroxides</data>
+    </node>
+    <node id="26">
+      <data key="d0">Phenolphthaleins</data>
+    </node>
+    <node id="27">
+      <data key="d0">Phenols (Acute toxicity)</data>
+    </node>
+    <node id="29">
+      <data key="d0">Phosphinate Esters (Acute toxicity)</data>
+    </node>
+    <node id="31">
+      <data key="d0">Thiols (Acute toxicity)</data>
+    </node>
+    <node id="32">
+      <data key="d0">Substituted Triazines (Acute toxicity)</data>
+    </node>
+    <node id="33">
+      <data key="d0">Triarylmethane Pigments/Dyes with Non-solubilizing Groups</data>
+    </node>
+    <node id="34">
+      <data key="d0">Vinyl Esters (Acute toxicity)</data>
+    </node>
+    <node id="35">
+      <data key="d0">Vinyl Sulfones</data>
+    </node>
+    <node id="37">
+      <data key="d0">Anhydrides, Carboxylic acid</data>
+    </node>
+    <node id="39">
+      <data key="d0">Diisocyanates</data>
+    </node>
+    <node id="40">
+      <data key="d0">Alkoxysilanes</data>
+    </node>
+    <node id="41">
+      <data key="d0">Dianilines</data>
+    </node>
+    <node id="42">
+      <data key="d0">Anionic Surfactants</data>
+    </node>
+    <node id="43">
+      <data key="d0">Dithiocarbamates (Acute toxicity)</data>
+    </node>
+    <node id="45">
+      <data key="d0">Polynitroaromatics (Acute toxicity)</data>
+    </node>
+    <node id="46">
+      <data key="d0">Rosin</data>
+    </node>
+    <node id="48">
+      <data key="d0">Aminobenzothiazole Azo Dyes</data>
+    </node>
+    <node id="49">
+      <data key="d0">Acrylates/Methacrylates (Chronic toxicity)</data>
+    </node>
+    <node id="50">
+      <data key="d0">Aldehydes (Chronic toxicity)</data>
+    </node>
+    <node id="51">
+      <data key="d0">Anilines (Chronic toxicity)</data>
+    </node>
+    <node id="53">
+      <data key="d0">Benzotriazoles (Chronic toxicity)</data>
+    </node>
+    <node id="55">
+      <data key="d0">Esters (Chronic toxicity)</data>
+    </node>
+    <node id="56">
+      <data key="d0">Imides (Chronic toxicity)</data>
+    </node>
+    <node id="58">
+      <data key="d0">Phenols (Chronic toxicity)</data>
+    </node>
+    <node id="61">
+      <data key="d0">Substituted Triazines (Chronic toxicity)</data>
+    </node>
+    <node id="62">
+      <data key="d0">Thiols (Chronic toxicity)</data>
+    </node>
+    <node id="63">
+      <data key="d0">Vinyl Esters (Chronic toxicity)</data>
+    </node>
+    <node id="65">
+      <data key="d0">Ethylene Glycol Ethers</data>
+    </node>
+    <edge source="1" target="7" />
+    <edge source="1" target="27" />
+    <edge source="1" target="45" />
+    <edge source="1" target="16" />
+    <edge source="1" target="21" />
+    <edge source="1" target="37" />
+    <edge source="3" target="9" />
+    <edge source="3" target="10" />
+    <edge source="3" target="16" />
+    <edge source="3" target="27" />
+    <edge source="3" target="21" />
+    <edge source="3" target="42" />
+    <edge source="3" target="15" />
+    <edge source="3" target="37" />
+    <edge source="3" target="34" />
+    <edge source="3" target="32" />
+    <edge source="3" target="40" />
+    <edge source="3" target="18" />
+    <edge source="4" target="21" />
+    <edge source="4" target="27" />
+    <edge source="4" target="16" />
+    <edge source="4" target="7" />
+    <edge source="4" target="11" />
+    <edge source="4" target="42" />
+    <edge source="4" target="23" />
+    <edge source="5" target="16" />
+    <edge source="5" target="21" />
+    <edge source="6" target="16" />
+    <edge source="7" target="27" />
+    <edge source="7" target="16" />
+    <edge source="7" target="32" />
+    <edge source="7" target="35" />
+    <edge source="7" target="41" />
+    <edge source="7" target="31" />
+    <edge source="7" target="45" />
+    <edge source="7" target="55" />
+    <edge source="7" target="17" />
+    <edge source="7" target="26" />
+    <edge source="7" target="37" />
+    <edge source="7" target="19" />
+    <edge source="7" target="40" />
+    <edge source="8" target="40" />
+    <edge source="9" target="10" />
+    <edge source="9" target="27" />
+    <edge source="9" target="16" />
+    <edge source="9" target="45" />
+    <edge source="10" target="27" />
+    <edge source="10" target="53" />
+    <edge source="10" target="55" />
+    <edge source="10" target="16" />
+    <edge source="10" target="58" />
+    <edge source="11" target="16" />
+    <edge source="15" target="19" />
+    <edge source="15" target="16" />
+    <edge source="15" target="21" />
+    <edge source="15" target="55" />
+    <edge source="15" target="18" />
+    <edge source="15" target="40" />
+    <edge source="15" target="32" />
+    <edge source="16" target="27" />
+    <edge source="16" target="45" />
+    <edge source="16" target="21" />
+    <edge source="16" target="37" />
+    <edge source="16" target="48" />
+    <edge source="16" target="32" />
+    <edge source="16" target="26" />
+    <edge source="16" target="40" />
+    <edge source="16" target="31" />
+    <edge source="16" target="42" />
+    <edge source="16" target="34" />
+    <edge source="16" target="29" />
+    <edge source="16" target="18" />
+    <edge source="16" target="25" />
+    <edge source="17" target="43" />
+    <edge source="17" target="45" />
+    <edge source="17" target="27" />
+    <edge source="17" target="18" />
+    <edge source="17" target="21" />
+    <edge source="17" target="19" />
+    <edge source="18" target="56" />
+    <edge source="18" target="58" />
+    <edge source="18" target="55" />
+    <edge source="18" target="32" />
+    <edge source="18" target="21" />
+    <edge source="19" target="39" />
+    <edge source="19" target="37" />
+    <edge source="19" target="40" />
+    <edge source="21" target="55" />
+    <edge source="21" target="27" />
+    <edge source="21" target="37" />
+    <edge source="21" target="58" />
+    <edge source="21" target="49" />
+    <edge source="21" target="26" />
+    <edge source="21" target="42" />
+    <edge source="21" target="34" />
+    <edge source="21" target="63" />
+    <edge source="21" target="50" />
+    <edge source="21" target="25" />
+    <edge source="21" target="23" />
+    <edge source="21" target="65" />
+    <edge source="21" target="46" />
+    <edge source="23" target="65" />
+    <edge source="25" target="42" />
+    <edge source="26" target="27" />
+    <edge source="27" target="31" />
+    <edge source="27" target="53" />
+    <edge source="27" target="55" />
+    <edge source="27" target="45" />
+    <edge source="27" target="49" />
+    <edge source="27" target="61" />
+    <edge source="27" target="33" />
+    <edge source="27" target="50" />
+    <edge source="27" target="32" />
+    <edge source="27" target="40" />
+    <edge source="31" target="42" />
+    <edge source="31" target="32" />
+    <edge source="31" target="55" />
+    <edge source="31" target="40" />
+    <edge source="32" target="35" />
+    <edge source="32" target="40" />
+    <edge source="32" target="39" />
+    <edge source="37" target="40" />
+    <edge source="39" target="56" />
+    <edge source="39" target="40" />
+    <edge source="42" target="55" />
+    <edge source="46" target="55" />
+    <edge source="49" target="55" />
+    <edge source="51" target="55" />
+    <edge source="53" target="55" />
+    <edge source="55" target="56" />
+    <edge source="55" target="58" />
+    <edge source="55" target="61" />
+    <edge source="55" target="63" />
+    <edge source="55" target="62" />
+    <edge source="58" target="61" />
+  </graph>
+</graphml>
